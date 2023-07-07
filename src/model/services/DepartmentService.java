95e0f192f6f735e7cc69788f07006100ba393628
Gabriel Giovani <gabrielgiovanesouza32@gmail.com>
2023-07-07 15:55:30 -0300

    Saving a new Department

--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -8,9 +8,18 @@ import model.entities.Department;
 
 public class DepartmentService {
 
-	DepartmentDao dao = DaoFactory.createDepartmentDao();
+	private DepartmentDao dao = DaoFactory.createDepartmentDao();
 	
 	public List<Department> findAll() {
 		return dao.findAll();
+	}
+	
+	public void saveOrUpdate(Department obj) {
+		if (obj.getId() == null) {
+			dao.insert(obj);
+		}
+		else {
+			dao.update(obj);
+		}
 	}
 }
